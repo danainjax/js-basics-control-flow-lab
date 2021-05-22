cbba57426e2ed736f3e9ee7255f141ee8af92c4a
danainjax <danainjax@gmail.com>
2021-05-22 15:14:46 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet<= 400) {
+    return 'This one is on me!';
+  } else if (feet < 2500 && feet > 1999) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  if (city != 'NYC') {
+    return 'No go.';
+  } else {
+    return 'Ok, sounds good.';
+  }
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+    return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.';
+      default:
+        return 'Bye.';
+  }
+    
+  
   // Write your code here!
 }
